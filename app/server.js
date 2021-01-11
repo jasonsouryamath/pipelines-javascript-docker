70611900aa871d21dc517ee7da65bed5c758a49e
jasonsouryamath <jasonsouryamath@yahoo.com>
2021-01-11 13:58:50 -0800

    Update server.js
    testing build triggers

--- app/server.js
+++ app/server.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0';
 
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello Architecture Refresh Team! :) \n');
+  res.send('Hello Architecture Refresh Team! attempting to test build triggers :) \n');
 });
 
 app.listen(PORT, HOST);
